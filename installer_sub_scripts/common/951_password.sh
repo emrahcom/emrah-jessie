7fe40028a84b09de676f84ed1e7a51cd8cfb0c0a
emrah <emrah.com@gmail.com>
2016-10-03 13:28:22 +0300

    print poweradmin's password if it's in the installation list

--- installer_sub_scripts/common/951_password.sh
+++ installer_sub_scripts/common/951_password.sh
@@ -13,11 +13,12 @@ echo "-------------------- STATUS --------------------"
 # -----------------------------------------------------------------------------
 # POWERADMIN
 # -----------------------------------------------------------------------------
-if [ "$DONT_RUN_POWERDNS" != true ]; then
-POWERADMIN_WEB_PASSWORD=$(grep "^\$db_pass" \
+if [ -v DONT_RUN_POWERDNS -a "$DONT_RUN_POWERDNS" != true ]
+then
+  POWERADMIN_WEB_PASSWORD=$(grep "^\$db_pass" \
   /var/lib/lxc/ej-powerdns/rootfs/var/www/html/poweradmin/inc/config.inc.php \
   | cut -d "'" -f2 | cut -c 1-20)
-echo "Poweradmin   : admin / $POWERADMIN_WEB_PASSWORD"
+  echo "Poweradmin   : admin / $POWERADMIN_WEB_PASSWORD"
 fi
 
 echo
