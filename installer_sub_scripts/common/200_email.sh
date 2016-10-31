a841ce05196314afda729b7cdcb1be28cc35110b
emrah <emrah.com@gmail.com>
2016-10-31 15:07:43 +0300

    ej-email: vexim acl conf

--- installer_sub_scripts/common/200_email.sh
+++ installer_sub_scripts/common/200_email.sh
@@ -225,6 +225,7 @@ lxc-attach -n $MACH -- \
          /var/www/html/vexim/config/variables.php"
 
 # customization for exim4
+cp etc/exim4/vexim-acl-*.conf  $ROOTFS/etc/exim4/
 cp -r etc/exim4/conf.d/*  $ROOTFS/etc/exim4/conf.d/
 
 lxc-attach -n $MACH -- \
